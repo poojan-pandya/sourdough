8128f4bbeeffee9ee5dfe46cc00922f1992a8f2b
Poojan Pandya <poojanpandya01@gmail.com>
2023-07-15 18:02:09 -0700

    a lot

--- App.js
+++ App.js
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ThisMonthScreen from './src/screens/ThisMonthScreen';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import AddScreen from './src/screens/AddScreen';
-import CategoryScreen from './src/screens/CategoryScreen';
+import AddScreenStack from './src/screens/AddScreenStack';
+import ThisMonthScreenStack from './src/screens/ThisMonthScreenStack';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,9 +11,8 @@ function App() {
   return (
       <NavigationContainer>
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-          <Tab.Screen name="This Month" component={ThisMonthScreen} />
-          <Tab.Screen name="Add" component={AddScreen} />
-          <Tab.Screen name="Category Screen" component={CategoryScreen}/>
+          <Tab.Screen name="This Month" component={ThisMonthScreenStack} />
+          <Tab.Screen name="Add" component={AddScreenStack} />
         </Tab.Navigator>
       </NavigationContainer>
   );
