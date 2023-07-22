27c905bf0d49303400a440bc07e0f1632c4d264a
Poojan Pandya <poojanpandya01@gmail.com>
2023-07-22 13:39:28 -0700

    Added icons to bottom tab bar

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AddScreenStack from './src/screens/AddScreenStack';
 import ThisMonthScreenStack from './src/screens/ThisMonthScreenStack';
 import HistoryScreenStack from './src/screens/HistoryScreenStack';
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { colors } from './src/styles/baseStyles';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,7 +13,33 @@ function App() {
 
   return (
       <NavigationContainer>
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'This Month') {
+              iconName = focused
+                ? 'calendar'
+                : 'calendar-outline';
+            } else if (route.name === 'Add') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
+            } else if (route.name === 'History') {
+              iconName = focused ? 'ios-list' : 'ios-list-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: colors.blue,
+          tabBarInactiveTintColor: colors.gray,
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: '#f2f2f2',
+            borderTopWidth: 0,
+          },
+        })}
+        >
           <Tab.Screen name="This Month" component={ThisMonthScreenStack} />
           <Tab.Screen name="Add" component={AddScreenStack} />
           <Tab.Screen name="History" component={HistoryScreenStack}/>
