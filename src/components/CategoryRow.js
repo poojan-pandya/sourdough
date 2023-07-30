36ae49b7d55100d8a4e541dc41bb73a2c88389d6
Poojan Pandya <poojanpandya01@gmail.com>
2023-07-30 10:53:39 -0700

    Make income row non-touchable

--- src/components/CategoryRow.js
+++ src/components/CategoryRow.js
@@ -1,79 +1,129 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { baseStyles, colors, padding } from '../styles/baseStyles'
-import React, { useContext } from 'react'
-import { totalEarnedThisMonth } from '../logic/calculations'
-import { useFocusEffect } from '@react-navigation/native'
-import { totalSpentForCategoryForMonth } from '../logic/history'
-import TransactionContext from '../context/TransactionContext'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { baseStyles, colors, padding } from "../styles/baseStyles";
+import React, { useContext } from "react";
+import { totalEarnedThisMonth } from "../logic/calculations";
+import { useFocusEffect } from "@react-navigation/native";
+import { totalSpentForCategoryForMonth } from "../logic/history";
+import TransactionContext from "../context/TransactionContext";
 
 const CategoryRow = ({ text, limit, emoji, onPress }) => {
+	const { allTransactions, setAllTransactions } =
+		useContext(TransactionContext);
 
-    const { allTransactions, setAllTransactions } = useContext(TransactionContext);
+	if (text === "Income") {
+		const [amount, setAmount] = React.useState(0);
+		useFocusEffect(
+			React.useCallback(() => {
+				totalEarnedThisMonth()
+					.then((total) => {
+						setAmount(total);
+					})
+					.catch((error) => {
+						console.log(error);
+					});
+			}, [allTransactions.length])
+		);
+		return (
+			<View
+				style={{ ...styles.card, backgroundColor: colors.lightGreen }}
+			>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+					}}
+				>
+					<View
+						style={{
+							flex: 1,
+							flexDirection: "column",
+							justifyContent: "space-between",
+							height: 60,
+						}}
+					>
+						<Text style={styles.bold_p}>{text}</Text>
+						<Text style={{ ...styles.bold_p }}>${amount}</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "col",
+							justifyContent: "center",
+						}}
+					>
+						<Text style={styles.h1}>{emoji}</Text>
+					</View>
+				</View>
+			</View>
+		);
+	}
 
-    if (text === "Income") {
-        const [amount, setAmount] = React.useState(0);
-        useFocusEffect(React.useCallback(() => {
-            totalEarnedThisMonth().then((total) => {
-                setAmount(total);
-            }).catch((error) => {
-                console.log(error);
-            });
-        }, [allTransactions.length]));
-        return (
-            <TouchableOpacity onPress={onPress}>
-            <View style={{...styles.card, backgroundColor: colors.lightGreen}}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                    <View style={{flex:1, flexDirection: 'column', justifyContent: 'space-between', height:60}}>
-                        <Text style={styles.bold_p}>{text}</Text>
-                        <Text style={{...styles.bold_p}}>${amount}</Text>
-                    </View>
-                    <View style={{flexDirection: 'col', justifyContent: "center"}}>
-                        <Text style={styles.h1}>{emoji}</Text>
-                    </View>
-                </View>
-            </View>
-        </TouchableOpacity>
-        )
-    }
+	const [amount, setAmount] = React.useState(0);
 
-    const [amount, setAmount] = React.useState(0);
+	useFocusEffect(
+		React.useCallback(() => {
+			const month = new Date().getMonth();
+			const year = new Date().getFullYear();
+			totalSpentForCategoryForMonth(text, month, year)
+				.then((total) => {
+					setAmount(total);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		}, [allTransactions.length])
+	);
 
-    useFocusEffect(React.useCallback(() => {
-        const month = new Date().getMonth();
-        const year = new Date().getFullYear();
-        totalSpentForCategoryForMonth(text, month, year).then((total) => {
-            setAmount(total);
-        }).catch((error) => {
-            console.log(error);
-        });
-    }, [allTransactions.length]));
+	return (
+		<TouchableOpacity onPress={onPress}>
+			<View style={styles.card}>
+				<View
+					style={{
+						flexDirection: "row",
+						justifyContent: "space-between",
+					}}
+				>
+					<View
+						style={{
+							flex: 1,
+							flexDirection: "column",
+							justifyContent: "space-between",
+							height: 60,
+						}}
+					>
+						<Text style={styles.bold_p}>{text}</Text>
+						<Text style={{ ...styles.bold_p, color: colors.gray }}>
+							{" "}
+							<Text
+								style={{ ...styles.bold_p, color: colors.blue }}
+							>
+								${amount}
+							</Text>{" "}
+							/ ${limit}
+						</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "col",
+							justifyContent: "center",
+						}}
+					>
+						<Text style={styles.h1}>{emoji}</Text>
+					</View>
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
+};
 
-  return (
-    <TouchableOpacity onPress={onPress}>
-        <View style={styles.card}>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                <View style={{flex:1, flexDirection: 'column', justifyContent: 'space-between', height:60}}>
-                    <Text style={styles.bold_p}>{text}</Text>
-                    <Text style={{...styles.bold_p, color:colors.gray}}> <Text style={{...styles.bold_p, color:colors.blue}}>${amount}</Text> / ${limit}</Text>
-                </View>
-                <View style={{flexDirection: 'col', justifyContent: "center"}}>
-                    <Text style={styles.h1}>{emoji}</Text>
-                </View>
-            </View>
-        </View>
-    </TouchableOpacity>
-  )
-}
-
-export default CategoryRow
+export default CategoryRow;
 
 const styles = StyleSheet.create({
-    ...baseStyles,
-    card: {
-        backgroundColor: colors.lightGray,
-        padding: padding.sm,
-        borderRadius: 10,
-        marginBottom: 10,
-        height: 80
-    }
-})
+	...baseStyles,
+	card: {
+		backgroundColor: colors.lightGray,
+		padding: padding.sm,
+		borderRadius: 10,
+		marginBottom: 10,
+		height: 80,
+	},
+});
